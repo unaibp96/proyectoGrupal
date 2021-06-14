5602de104bac2fb1a2e9fe71f6414cc884cd31b8
Unai Barrocal Perez <unai.barrocal@campusfp.es>
2021-06-14 13:54:01 +0200

    añadiendo firebase
    Firebase

--- js/cookies.js
+++ js/cookies.js
@@ -1,7 +1,7 @@
 function guardarCookie(nombre,valor,fecha) {
     document.cookie = nombre+"="+valor+";expires="+fecha;
     }
-caduca = "31 Dec 2020 23:59:59 GMT"
+caduca = "31 Dec 2021 23:59:59 GMT"
 function guardar() {
     tuCookie = "pruebaCookie"
     tuValor = document.dato.miCookie.value

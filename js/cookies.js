4c839fc41ac191a203c1042e315a61d6218709f1
Unai Barrocal Perez <unai.barrocal@campusfp.es>
2021-06-14 13:45:30 +0200

    aqui añado las cookies
    cookies

---
+++ js/cookies.js
@@ -0,0 +1,9 @@
+function guardarCookie(nombre,valor,fecha) {
+    document.cookie = nombre+"="+valor+";expires="+fecha;
+    }
+caduca = "31 Dec 2020 23:59:59 GMT"
+function guardar() {
+    tuCookie = "pruebaCookie"
+    tuValor = document.dato.miCookie.value
+    guardarCookie(tuCookie,tuValor,caduca)	 
+    }
